e4f0abd1a3d9130e4a5680c70fecb4e9e1279d32
julia-piskoha <upiskoha@gmail.com>
2023-02-27 22:05:51 +0200

    searchBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import Searchbar from './Searchbar/Searchbar';
+
+export default class App extends React.Component {
+  state = {
+    imageName: '',
+  };
+  handleFormSubmit = imageName => {
+    this.setState({ imageName });
+  };
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.handleFormSubmit} />;
+        <ToastContainer autoClose={3000} />;
+      </>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+import css from './Searchbar.module.css';
+
+export default class Searchbar extends React.Component {
+  state = {
+    imageName: '',
+  };
+  handleNameChange = e => {
+    this.setState({ imageName: e.currentTarget.value.toLowerCase() });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.imageName.trim() === '') {
+      toast('Enter a request');
+      return;
+    }
+    this.props.onSubmit(this.state.imageName);
+    this.setState({ imageName: '' });
+  };
+  render() {
+    return (
+      <header className={css.searchbar}>
+        <form onSubmit={this.handleSubmit} className={css.form}>
+          <button type="submit" className={css.button}>
+            <span className={css.button_label}>Search</span>
+          </button>
+
+          <input
+            className={css.input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            name="imageName"
+            value={this.state.imageName}
+            onChange={this.handleNameChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
